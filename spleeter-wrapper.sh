d795c0543f3b812f1e14829481180c9a72920d39
Magne Matre Gåsland <redbar0n@users.noreply.github.com>
2021-05-14 14:43:57 +0200

    Doc: Briefly state license, and move full license to bottom of file.

--- spleeter-wrapper.sh
+++ spleeter-wrapper.sh
@@ -5,20 +5,8 @@
 #
 #     Author: Amaury Bodet
 #
-#     ---
-#     This program is free software: you can redistribute it and/or modify
-#     it under the terms of the GNU General Public License as published by
-#     the Free Software Foundation, either version 3 of the License, or
-#     (at your option) any later version.
+#     License: GNU GPL v3 or later version. (See bottom of this file for entire license notice.)
 #
-#     This program is distributed in the hope that it will be useful,
-#     but WITHOUT ANY WARRANTY; without even the implied warranty of
-#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#     GNU General Public License for more details.
-#
-#     You should have received a copy of the GNU General Public License
-#     along with this program.  If not, see <https://www.gnu.org/licenses/>.
-#     ---
 #
 #     Cracks:
 #     "
@@ -294,3 +282,19 @@ fi
 
 # deactivate anaconda / miniconda
 conda deactivate
+
+
+#     --- License notice:
+#     This program is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+#
+#     This program is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+#
+#     You should have received a copy of the GNU General Public License
+#     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#     ---
